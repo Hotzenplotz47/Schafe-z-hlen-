b17d75253ac75b07e551c27329ce1101e3eeae2c
Hotzenplotz47 <paul2963572125@gmail.com>
2025-02-01 00:37:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,60 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
+
+
+
+
+
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCfFrvGZ2esnAuDRhj1SG8XfcG0G3N5zF8",
+
+  authDomain: "schafe-df870.firebaseapp.com",
+
+  databaseURL: "https://schafe-df870-default-rtdb.firebaseio.com",
+
+  projectId: "schafe-df870",
+
+  storageBucket: "schafe-df870.firebasestorage.app",
+
+  messagingSenderId: "741265220339",
+
+  appId: "1:741265220339:web:3a0b7c4d12cb9dcab80d86",
+
+  measurementId: "G-828WEWWYC8"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+
+
+
+
+
+
+
+
 // DOM-Elemente
 const sheepCountInput = document.getElementById('sheepCount');
 const submitButton = document.getElementById('submitButton');
